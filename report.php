de2d63a9955bdc253eb2de79a94c44f05ffeff77
Larry Garfield <larry@garfieldtech.com>
2015-08-24 14:10:08 -0500

    Add top-speakers report.

--- report.php
+++ report.php
@@ -9,22 +9,39 @@ use Crell\HtmlModel\HtmlPage;
  */
 function report()
 {
+    $page = new HtmlPage();
+    $page = $page->withTitle('Conference audit');
+
     // Generate the results page.
     $table = reportNewSpeakersPerCon();
+    $page = $page->withContent($page->getContent() . $table);
 
-    $page = new HtmlPage($table);
-    $page->withTitle('Conference audit');
+    $table = reportTopSpeakers();
+    $page = $page->withContent($page->getContent() . $table);
 
     file_put_contents('results.html', $page);
+}
+
+function reportTopSpeakers()
+{
+    $conn = getDb();
+
+    $stmt = $conn->executeQuery("SELECT speaker, COUNT(speaker) AS appearances
+        FROM talk
+          WHERE speaker <> ''
+        GROUP BY speaker
+        HAVING appearances >= 20
+        ORDER BY appearances DESC, speaker DESC");
+
+    $header = ['Speaker', 'Appearances (since 2011)'];
+
+    return makeHtmlTable('Most popular speakers', $header, $stmt->fetchAll());
 }
 
 
 function reportNewSpeakersPerCon()
 {
     $conn = getDb();
-
-    $queryBuilder = $conn->createQueryBuilder();
-
 
     $stmt = $conn->executeQuery("SELECT event.start_date, event.name, talks_count, num_speakers, new_speakers, FORMAT((new_speakers/event.num_speakers)*100, 1) AS percent_new
         FROM event
@@ -45,7 +62,5 @@ function reportNewSpeakersPerCon()
 
     return $table . "<p>Average new speaker percentage: {$average}</p>\n";
 }
-
-
 
 report();
